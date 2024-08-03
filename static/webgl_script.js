cdf2505747610e20fba389f860daad4b3f98a94a
nazgull08 <nazgull08@protonmail.com>
2024-08-03 21:39:39 +0300

    added sheeps

--- static/webgl_script.js
+++ static/webgl_script.js
@@ -1,9 +1,40 @@
-//import init, { start } from "./pkg/rust_webgl_renderer.js";
-import init, { start } from "./pkg/vaporization.js";
+import init, { run_app } from './pkg/vaporization.js';
 
 async function run() {
-    await init();
-    start();
+    await init(); // Загружаем WASM-модуль
+    run_app(); // Запускаем приложение, которое должно создать канвас
+
+    const observer = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+            mutation.addedNodes.forEach(node => {
+                if (node.nodeName === 'CANVAS') {
+                    resizeCanvas(node); // Настроить размер сразу после добавления
+                    observer.disconnect(); // Отключаем наблюдатель после настройки
+                    document.getElementById('loading').style.display = 'none'; // Скрываем полосу загрузки
+                    
+                    // Вешаем обработчик на canvas для активации Pointer Lock
+                    node.addEventListener('click', () => {
+                        if (document.pointerLockElement !== node) {
+                            node.requestPointerLock();
+                        }
+                    });
+                }
+            });
+        });
+    });
+    observer.observe(document.body, { childList: true });
 }
+
+function resizeCanvas(canvas) {
+    canvas.style.width = '100%';
+    canvas.style.height = '100%';
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+window.addEventListener('resize', () => {
+    const canvas = document.querySelector('canvas');
+    if (canvas) resizeCanvas(canvas);
+});
 
 run();
